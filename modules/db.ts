33d2fc8495c2811533cc828feb38ae1b35325b81
Felix <49159678+FelixYizhu@users.noreply.github.com>
2020-10-28 15:51:30 +1100

    Add files via upload

--- modules/db.ts
+++ modules/db.ts
@@ -1,1 +1,12 @@
+export var dbUrl=''
 
+export function getDbData():any[]{
+    console.log('get data from DB');
+    return[
+        {title:'123'},
+        {type:'345'}
+    ]
+}
+export function save(){
+    console.log('save successfully');
+}
