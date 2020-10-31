65b2d46e292f5169e8a21d154c77d29579e0df07
Felix <49159678+FelixYizhu@users.noreply.github.com>
2020-10-31 22:25:44 +1100

    Add files via upload

--- index.ts
+++ index.ts
@@ -5,11 +5,11 @@ function handleClick(){
     console.log('click me to try again');
 }
 let btn=document.querySelector('#btn') as HTMLButtonElement;
-btn.addEventListener('click',handleClick)
+btn.addEventListener('click',handleClick,{once:true})
 
 let remove=document.querySelector('#remove') as HTMLButtonElement;
 remove.addEventListener("click",function(){
-    btn.removeEventListener('click',handleClick)
+    btn.removeEventListener('click',handleClick,)
     console.log('remove successfully');
 })
 
